ae6c749f465c0887eb0270606095f76cfb0b980f
Ivan Winslow <ivan.winslow@errepar.com>
2022-06-01 08:51:13 -0300

    serverWorker Setup

---
+++ client/src/mocks/handlers.js
@@ -0,0 +1,21 @@
+// src/mocks/handlers.js
+import { rest } from 'msw'
+
+export const handlers =[
+    //handlerType.method('routeURL', (req, res,ctx)=>{})
+    rest.get("http://localhost:3030/scoops", (req, res, ctx)=>{
+        return res(
+            ctx.status(200),
+            ctx.json([
+                {
+                "name": "Mint chip",
+                "imagePath": "/images/mint-chip.png" 
+                },
+                {
+                "name": "Vanilla",
+                "imagePath": "/images/vanilla.png" 
+                },
+            ])
+        )
+    })
+]
